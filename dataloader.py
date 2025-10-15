1bb0186410876909ba211866c7020102a5aefd99
z4lt <adrianwong265@gmail.com>
2025-10-15 23:31:11 +1100

    Implement PyTorch Dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,35 +1,41 @@
 from torch.utils.data import DataLoader, Dataset, random_split
-import feature_manipulation
+import librosa
+import os
+import audio_util
+import glob
 
-class SoundDS(Dataset):
-  def __init__(self, df, data_path):
-    self.df = df
-    self.data_path = str(data_path)
+class AudioDatasetLoader(Dataset):
+  def __init__(self, data_path):
+    self.data_path = data_path
+    self.audio_files = sorted(glob.glob(os.path.join(data_path, "*.wav")))
     self.duration = 3333
     self.sr = 48000
     self.channel = 1
     self.shift_pct = 0.4
             
   def __len__(self):
-    return len(self.df)    
+    return len(self.audio_files)    
     
   def __getitem__(self, idx):
-    audio_file = self.data_path + self.df.loc[idx, 'relative_path']
-    # Get the Class ID
-    class_id = self.df.loc[idx, 'classID']
+    audio_file = self.audio_files[idx]
+    basename = os.path.basename(audio_file)
+    class_id = basename.split('_')[0]
 
-    aud = AudioUtil.open(audio_file)
-    # Some sounds have a higher sample rate, or fewer channels compared to the
-    # majority. So make all sounds have the same number of channels and same 
-    # sample rate. Unless the sample rate is the same, the pad_trunc will still
-    # result in arrays of different lengths, even though the sound duration is
-    # the same.
-    reaud = AudioUtil.resample(aud, self.sr)
-    rechan = AudioUtil.rechannel(reaud, self.channel)
+    y, sr = librosa.load(audio_file, sr=48000, mono=True)
 
-    dur_aud = AudioUtil.pad_trunc(rechan, self.duration)
-    shift_aud = AudioUtil.time_shift(dur_aud, self.shift_pct)
-    sgram = AudioUtil.spectro_gram(shift_aud, n_mels=64, n_fft=1024, hop_len=None)
-    aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
+    mel_spectrogram = audio_util.create_mel(y, sr)
+    aug_spectrogram = audio_util.time_shift(mel_spectrogram, 20)
+    aug_spectrogram = audio_util.time_mask(aug_spectrogram)
+    aug_spectrogram = audio_util.freq_mask(aug_spectrogram)
 
-    return aug_sgram, class_id
+    return aug_spectrogram, class_id
+  
+def main():
+  dataset = AudioDatasetLoader("./output")
+
+  for i in range(len(dataset)):
+      spectrogram, class_id = dataset[i]
+      print(f"Loaded {class_id} with shape {spectrogram.shape}")
+
+if __name__ == "__main__":
+  main()
